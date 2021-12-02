edeff0ff07d536994f2b81ff1b5a732ed095bb85
Isaac <joisizur@espol.edu.ec>
2021-12-02 03:33:11 -0500

    Correccion de errores. Creacion funciones estaticas nextCriterio y NextPremio

--- concursoMascotas/src/main/java/ec/edu/espol/model/Criterio.java
+++ concursoMascotas/src/main/java/ec/edu/espol/model/Criterio.java
@@ -16,6 +16,7 @@ import java.util.Scanner;
  * @author isaac
  */
 public class Criterio {
+
     private int id;
     private String descripcion;
     private ArrayList<Evaluacion> evaluaciones;
@@ -29,66 +30,106 @@ public class Criterio {
         this.idConcurso = idConcurso;
         this.concurso = concurso;
     }
+
+    public Criterio(String descripcion, int idConcurso, Concurso concurso) {
+        this.id = Util.nextID("criterios");
+        this.descripcion = descripcion;
+        this.evaluaciones = new ArrayList<>();
+        this.idConcurso = idConcurso;
+        this.concurso = concurso;
+    }
+
     //getters
     public int getId() {
         return id;
     }
+
     public String getDescripcion() {
         return descripcion;
     }
+
     public ArrayList<Evaluacion> getEvaluaciones() {
         return evaluaciones;
     }
+
     public int getIdConcurso() {
         return idConcurso;
     }
+
     public Concurso getConcurso() {
         return concurso;
     }
-    
+
     //setters
     public void setId(int id) {
         this.id = id;
     }
+
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
+
     public void setEvaluaciones(ArrayList<Evaluacion> evaluaciones) {
         this.evaluaciones = evaluaciones;
     }
+
     public void setIdConcurso(int idConcurso) {
         this.idConcurso = idConcurso;
     }
+
     public void setConcurso(Concurso concurso) {
         this.concurso = concurso;
     }
-    
-    public void saveFile(String archivo){//esta en modo a(para añadir)
-        try(PrintWriter pw=new PrintWriter(new FileOutputStream(new File(archivo), true))){
-            pw.println(this.id+"|"+this.descripcion+"|"+this.idConcurso);
-        }
-        catch(Exception e){
+
+    public void saveFile(String archivo) {//esta en modo a(para añadir)
+        try (PrintWriter pw = new PrintWriter(new FileOutputStream(new File(archivo), true))) {
+            pw.println(this.id + "|" + this.descripcion + "|" + this.idConcurso);
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
-    
+
     //ARCHIVOS LECTURA
-    public static ArrayList<Criterio> readFile(String archivo){
-        ArrayList<Criterio> criterios=new ArrayList<>();//creo una lista de criterios
-        try(Scanner sc=new Scanner(new File(archivo))){
-           while(sc.hasNextLine()){//mientras exista la sguiente linea
-               String linea=sc.nextLine();
-               String[] datos=linea.split("|");
-               Criterio cr=new Criterio(Integer.parseInt(datos[0]),datos[1], Integer.parseInt(datos[2]), null);//se crea un objeto criterio
-               criterios.add(cr);
-           }
-        }
-        catch(Exception e){
+    public static ArrayList<Criterio> readFile(String archivo) {
+        ArrayList<Criterio> criterios = new ArrayList<>();//creo una lista de criterios
+        try (Scanner sc = new Scanner(new File(archivo))) {
+            while (sc.hasNextLine()) {//mientras exista la sguiente linea
+                String linea = sc.nextLine();
+                String[] datos = linea.split("|");
+                Criterio cr = new Criterio(Integer.parseInt(datos[0]), datos[1], Integer.parseInt(datos[2]), null);//se crea un objeto criterio
+                criterios.add(cr);
+            }
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        
+
         return criterios;
     }
-    
-    
+
+    //FUNCIONES ESTATICAS
+    public void nextCriterio(Scanner sc) {
+        System.out.println("Ingrese la cantidad de criterios para el concurso:");
+        int cantidad = sc.nextInt();
+        sc.nextLine();
+        while (cantidad <= 0) {
+            System.out.println("Ingrese un valor mayor a 0 para los criterios: ");
+            cantidad = sc.nextInt();
+            sc.nextLine();
+        }
+        int sumador = 0;//o contador
+        String[] descripciones = new String[cantidad];
+        while (sumador < cantidad) {
+            System.out.println("Ingrese la descripcion del criterio " + (sumador + 1) + ":");
+            String descrip = sc.nextLine();
+            descripciones[sumador] = descrip;
+        }
+        System.out.println("Ingrese el nombre del concurso: ");
+        String nombreConcurso = sc.nextLine();
+        Concurso valido = Concurso.anexarNombre(nombreConcurso);
+        for (int i = 0; i < cantidad; i++) {
+            Criterio p = new Criterio(descripciones[i], valido.getId(), valido);
+            p.saveFile("premios.txt");
+        }
+    }
+
 }
--- concursoMascotas/src/main/java/ec/edu/espol/model/Premio.java
+++ concursoMascotas/src/main/java/ec/edu/espol/model/Premio.java
@@ -8,6 +8,7 @@ package ec.edu.espol.model;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.PrintWriter;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -16,6 +17,7 @@ import java.util.Scanner;
  * @author gerar
  */
 public class Premio {
+
     private int id;
     private int lugar;
     private String descripcion;
@@ -29,101 +31,106 @@ public class Premio {
         this.idConcurso = idConcurso;
         this.concurso = concurso;
     }
+
     public Premio(int lugar, String descripcion, int idConcurso, Concurso concurso) {
+        this.id = Util.nextID("premios.txt");
         this.lugar = lugar;
         this.descripcion = descripcion;
         this.idConcurso = idConcurso;
         this.concurso = concurso;
     }
-    
+
     //getters
     public int getId() {
         return id;
     }
+
     public int getLugar() {
         return lugar;
     }
+
     public String getDescripcion() {
         return descripcion;
     }
+
     public Concurso getConcurso() {
         return concurso;
     }
+
     public int getIdConcurso() {
         return idConcurso;
     }
-    
-    
+
     //setters
     public void setId(int id) {
         this.id = id;
     }
+
     public void setLugar(int lugar) {
         this.lugar = lugar;
     }
+
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
+
     public void setConcurso(Concurso concurso) {
         this.concurso = concurso;
     }
+
     public void setIdConcurso(int idConcurso) {
         this.idConcurso = idConcurso;
     }
-    
+
     //ARCHIVOS ECRITURA
-    public void saveFile(String archivo){//esta en append 
-        try(PrintWriter pw=new PrintWriter(new FileOutputStream(new File(archivo), true))){
-            pw.println(this.id+"|"+this.lugar+"|"+this.descripcion+"|"+this.idConcurso+"|"+this.concurso.toString());
-        }
-        catch(Exception e){
+    public void saveFile(String archivo) {//esta en append 
+        try (PrintWriter pw = new PrintWriter(new FileOutputStream(new File(archivo), true))) {
+            pw.println(this.id + "|" + this.lugar + "|" + this.descripcion + "|" + this.idConcurso + "|" + this.concurso.toString());
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
- 
+
     //ARCHIVOS LECTURA
-    public static ArrayList<Premio> readFile(String archivo){
-        ArrayList<Premio> listaPremio=new ArrayList<>();
-        try(Scanner sc=new Scanner(new File(archivo))){
-           while(sc.hasNextLine()){//mientras exista la sguiente linea
-               String linea=sc.nextLine();
-               String[] datos=linea.split("|");
-               Premio p=new Premio(Integer.parseInt(datos[0]), Integer.parseInt(datos[1]), datos[2], Integer.parseInt(datos[3]), null) ;//se crea un objeto premio
-               listaPremio.add(p);
-           }
-        }
-        catch(Exception e){
+    public static ArrayList<Premio> readFile(String archivo) {
+        ArrayList<Premio> listaPremio = new ArrayList<>();
+        try (Scanner sc = new Scanner(new File(archivo))) {
+            while (sc.hasNextLine()) {//mientras exista la sguiente linea
+                String linea = sc.nextLine();
+                String[] datos = linea.split("|");
+                Premio p = new Premio(Integer.parseInt(datos[0]), Integer.parseInt(datos[1]), datos[2], Integer.parseInt(datos[3]), new Concurso(Integer.parseInt(datos[4]), datos[5], datos[6], Double.parseDouble(datos[7]), LocalDate.parse(datos[8]), LocalDate.parse(datos[9]), LocalDate.parse(datos[10])));//se crea un objeto premio
+                listaPremio.add(p);
+            }
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        
+
         return listaPremio;
     }
-    
-    public void nextPremio(Scanner sc){
+
+    public void nextPremio(Scanner sc) {
         System.out.println("Ingrese la cantidad de premios para el concurso:");
-                int cantidad = sc.nextInt();
-                sc.nextLine();
-                while(cantidad<=0){
-                    System.out.println("Ingrese un valor mayor a 0 para los premios: ");
-                    cantidad = sc.nextInt();
-                    sc.nextLine();
-                }
-                int sumador = 0;//o contador
-                String[] descripciones = new String[cantidad];
-                int[] idPremio = new int[cantidad];
-                while (sumador < cantidad) {
-                    System.out.println("Ingrese la descripcion del premio " + (sumador + 1) + ":");
-                    String descrip = sc.nextLine();
-                    descripciones[sumador] = descrip;
-                    idPremio[sumador] = sumador + 1;
-                }
-                System.out.println("Ingrese el nombre del concurso: ");
-                String nombreConcurso = sc.nextLine();
-                Concurso valido=Concurso.anexarNombrePremio(nombreConcurso);
-                for(int i=0;i<cantidad;i++){
-                    Premio p=new Premio(i+1,descripciones[i],valido.getId(),valido);
-                    p.saveFile("premios.txt");
-                }
+        int cantidad = sc.nextInt();
+        sc.nextLine();
+        while (cantidad <= 0) {
+            System.out.println("Ingrese un valor mayor a 0 para los premios: ");
+            cantidad = sc.nextInt();
+            sc.nextLine();
+        }
+        int sumador = 0;//o contador
+        String[] descripciones = new String[cantidad];
+        while (sumador < cantidad) {
+            System.out.println("Ingrese la descripcion del premio " + (sumador + 1) + ":");
+            String descrip = sc.nextLine();
+            descripciones[sumador] = descrip;
+        }
+        System.out.println("Ingrese el nombre del concurso: ");
+        String nombreConcurso = sc.nextLine();
+        Concurso valido = Concurso.anexarNombre(nombreConcurso);
+        for (int i = 0; i < cantidad; i++) {
+            Premio p = new Premio(i + 1, descripciones[i], valido.getId(), valido);
+            p.saveFile("premios.txt");
+        }
     }
-    
+
 }
